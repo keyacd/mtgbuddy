64e0216eec17a8af06089720a577a369665efbde
keyacd <darienkeyack@gmail.com>
2016-04-21 19:37:54 -0400

    Update messenger.py

--- bot/messenger.py
+++ bot/messenger.py
@@ -60,7 +60,7 @@ class Messenger(object):
                 "pretext": "Found "+get_name+"!",
                 "title": "View "+get_name+" on magiccards.info",
                 "title_link": card_url,
-                "text": img_url, #should be get_name, testing
+                "text": get_name
                 "fallback": card_name,
                 "image_url": img_url,
                 "color": "#7CD197",
