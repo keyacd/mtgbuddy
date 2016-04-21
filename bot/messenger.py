34aa61a4ff1e31b51d29820a5cbb0fc339fe6011
keyacd <darienkeyack@gmail.com>
2016-04-21 15:45:22 -0400

    Update messenger.py

--- bot/messenger.py
+++ bot/messenger.py
@@ -27,21 +27,27 @@ class Messenger(object):
     def write_prompt(self, channel_id, msg):
         bot_uid = self.clients.bot_user_id()
         card_name = msg.replace("@"+bot_uid, "")
-        card_name = msg.replace("<>", "")
-        txt = "Searching for "+card_name+"..."
-        # do the stuff to get the following things
-        get_name = "UNIMPLEMENTED"
-        get_id = "376404"
-        attachment = {
-            "pretext": "Found "+get_name+"!",
-            "title": "Gatherer info for "+get_name+":",
-            "title_link": "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid="+get_id,
-            "text": card_name,
-            "fallback": get_name,
-            "image_url": "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid="+get_id+"&type=card",
-            "color": "#7CD197",
-        }
-        self.clients.web.chat.post_message(channel_id, txt, attachments=[attachment], as_user='true')
+        card_name = card_name.replace("<>", "")
+        if card_name == "":
+            txt = '{}\n{}\n{}\n{}'.format(
+            "I can't find a card if you don't give me a name!",
+            "If you're confused, just type my name and then 'help?'")
+            self.send_message(channel_id, txt)
+        else:
+            txt = "Searching for "+card_name+"..."
+            # do the stuff to get the following things
+            get_name = "UNIMPLEMENTED"
+            get_id = "376404"
+            attachment = {
+                "pretext": "Found "+get_name+"!",
+                "title": "Gatherer info for "+get_name+":",
+                "title_link": "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid="+get_id,
+                "text": card_name,
+                "fallback": get_name,
+                "image_url": "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid="+get_id+"&type=card",
+                "color": "#7CD197",
+            }
+            self.clients.web.chat.post_message(channel_id, txt, attachments=[attachment], as_user='true')
 
     def write_error(self, channel_id, err_msg):
         txt = ":face_with_head_bandage: my maker didn't handle this error very well:\n>```{}```".format(err_msg)
