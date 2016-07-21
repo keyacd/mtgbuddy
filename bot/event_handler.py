e93622eaa75294f27804fdc7ed2ffa1df4f2e885
Kathryn DiPippo <dipipk@rpi.edu>
2016-07-21 13:09:23 -0400

    Edited event_handler.py to adjust handle_message
    At the moment it doesn't handle card statements. Hopefully with this
    change it will.

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -16,21 +16,18 @@ class RtmEventHandler(object):
 
     def _handle_by_type(self, event_type, event):
         # See https://api.slack.com/rtm for a full list of events
-        # In reality, mtgbuddy should only be conducting the message event
-        
-        #if event_type == 'error':
+        if event_type == 'error':
             # error
-            #self.msg_writer.write_error(event['channel'], json.dumps(event))
-        #elif event_type == 'message':
-        if event_type == 'message':
+            self.msg_writer.write_error(event['channel'], json.dumps(event))
+        elif event_type == 'message':
             # message was sent to channel
             self._handle_message(event)
-        #elif event_type == 'channel_joined':
+        elif event_type == 'channel_joined':
             # you joined a channel
-        #    self.msg_writer.write_help_message(event['channel'])
-        #elif event_type == 'group_joined':
+            self.msg_writer.write_help_message(event['channel'])
+        elif event_type == 'group_joined':
             # you joined a private group
-        #    self.msg_writer.write_help_message(event['channel'])
+            self.msg_writer.write_help_message(event['channel'])
         else:
             pass
 
@@ -41,12 +38,11 @@ class RtmEventHandler(object):
 
             if self.clients.is_bot_mention(msg_txt):
                 # e.g. user typed: "@pybot tell me a joke!"
-                msg_txt = msg_txt[11:] # takes out the name of the bot
-                
-                if 'help' in msg_txt:
+                if 'help?' in msg_txt:
                     self.msg_writer.write_help_message(event['channel'],msg_txt)
-                elif msg_txt.startswith('card'):
-                    #msg_txt[5:] only sends over the card name for the search
-                    self.msg_writer.write_prompt(event['channel'], msg_txt[5:])
+                elif 'card' in msg_txt:
+                    msg_txt = msg_txt.split(" ")
+                    msg_txt = " ".join(cardname[2:]
+                    self.msg_writer.write_prompt(event['channel'], msg_txt)
                 else:
                     pass
